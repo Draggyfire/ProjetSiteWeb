75841bc2f5c1ca2697d4262ba81e4639bc377375
Delsol Cameron <cameron_delsol@ens.univ-artois.fr>
2022-04-09 14:16:46 +0200

    :construction: Ajout d'une navbar

---
+++ projet/resources/views/layout/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav">
+                <a>Accueil</a>
+                <a>Liste</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
